94cc1f0a218c9938c85420aa212f849f14d7ae3c
ZhaiXchao <13739603637@163.com>
2019-12-23 15:19:28 +0800

    项目结构创建完成

---
+++ gmall-sms/src/test/java/com/zxc/gmall/sms/GmallSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxc.gmall.sms;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallSmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
